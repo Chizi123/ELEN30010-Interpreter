4d99a406154378a68f694afc0b25d95f23893db2
Chizi123 <joelgrun@gmail.com>
2019-05-13 15:37:14 +1000

    Minor reformatting, added project overview in README.org

--- assembler.c
+++ assembler.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-void line_parser(int ad3cnt, int ad2cnt, int ad1cnt, int cmdcnt, int ctr, char *addr3, char *addr2, char *addr1, char *cmd, char *line);
 void print_bits(FILE *out, int num);
-void print_where(FILE *out, char* arg);
+void print_where(FILE *out, char *arg);
 int print_loc(FILE *out, char *loc, int run);
 
 int main(int argc, char **argv)
 {
 	FILE *in, *out;
-	out = fopen(argv[2],"w");
-	in = fopen(argv[1],"r");
+	out = fopen(argv[2], "w");
+	in = fopen(argv[1], "r");
 	char line[1024];
-	int i_num=0,invalid_flag=0;
+	int i_num = 0, invalid_flag = 0;
 	for (int i = 0; i < 2; i++) {
 		if (i) {
-			fprintf(out,"`include \"CPU.vh\"\n\nmodule AsyncROM(input [7:0] addr,\n\toutput reg [34:0] data);\n");
-			fprintf(out,"\talways @(addr)\n\t\tcase(addr)\n");
+			fprintf(out, "`include \"CPU.vh\"\n\nmodule AsyncROM(input [7:0]addr,\n\toutput reg [34:0] data);\n");
+			fprintf(out, "\talways @(addr)\n\t\tcase(addr)\n");
 		}
 		while (fgets(line, 1024, in)) {
 			if (strlen(line) < 5)
 				continue;
 			char cmd[5] = "", addr1[10] = "", addr2[10] = "", addr3[10] = "";
-			int ctr=0,cmdcnt=0,ad1cnt=0,ad2cnt=0,ad3cnt=0;
+			int ctr = 0, cmdcnt = 0, ad1cnt = 0, ad2cnt = 0, ad3cnt = 0;
 			for (int i = 0; line[i]; i++) {
 				if (line[i] == '\t') {
 					continue;
@@ -35,180 +34,187 @@ int main(int argc, char **argv)
 				if (line[i] == ';' || line[i] == '/')
 					break;
 				switch (ctr) {
-				case 0: cmd[cmdcnt++] = line[i];
+				case 0:
+					cmd[cmdcnt++] = line[i];
 					break;
-				case 1: addr1[ad1cnt++] = line[i];
+				case 1:
+					addr1[ad1cnt++] = line[i];
 					break;
-				case 2: addr2[ad2cnt++] = line[i];
+				case 2:
+					addr2[ad2cnt++] = line[i];
 					break;
-				case 3: addr3[ad3cnt++] = line[i];
+				case 3:
+					addr3[ad3cnt++] = line[i];
 					break;
 				default:
 					break;
 				}
 			}
-			 cmd[cmdcnt] = '\0'; addr1[ad1cnt] = '\0'; addr2[ad2cnt] = '\0'; addr3[ad3cnt] = '\0';
+			cmd[cmdcnt] = '\0';
+			addr1[ad1cnt] = '\0';
+			addr2[ad2cnt] = '\0';
+			addr3[ad3cnt] = '\0';
 			if (cmdcnt == 3)
 				cmd[++cmdcnt] = '\0';
 			if (cmdcnt == 4)
 				cmd[++cmdcnt] = '\0';
 			if (i)
-				fprintf(out,"\t\t\t%d: data = {",i_num++);
-			switch (*((int *) cmd)) {
-			case 0x504f4e: //NOP (NOP)
+				fprintf(out, "\t\t\t%d: data = {", i_num++);
+			switch (*((int *)cmd)) {
+			case 0x504f4e: // NOP (NOP)
 				if (i)
-					fprintf(out,"35'b0");
+					fprintf(out, "35'b0");
 				break;
-			case 0x435441: //ATC (ATC)
+			case 0x435441: // ATC (ATC)
 				if (i) {
-					fprintf(out,"`ATC,");
-					invalid_flag=print_loc(out,addr1,i);
-					fprintf(out,"`N10,`N10,");
-					print_bits(out,atoi(addr2)-1);
+					fprintf(out, "`ATC,");
+					invalid_flag = print_loc(out, addr1, i);
+					fprintf(out, "`N10,`N10,");
+					print_bits(out, atoi(addr2) - 1);
 				}
 				break;
-			case 0x564f4d: //MOV PUR (MOV)
+			case 0x564f4d: // MOV PUR (MOV)
 				if (i) {
-					fprintf(out,"`MOV,`PUR,");
+					fprintf(out, "`MOV,`PUR,");
 					print_where(out, addr1);
 					print_where(out, addr2);
 					fprintf(out, "`N8");
 				}
 				break;
-			case 0x4c4853: //MOV SHL (SHL)
+			case 0x4c4853: // MOV SHL (SHL)
 				if (i) {
-					fprintf(out,"`MOV,`SHL,");
+					fprintf(out, "`MOV,`SHL,");
 					print_where(out, addr1);
 					print_where(out, addr2);
 					fprintf(out, "`N8");
 				}
 				break;
-			case 0x524853: //MOV SHR (SHR)
+			case 0x524853: // MOV SHR (SHR)
 				if (i) {
-					fprintf(out,"`MOV,`SHR,");
+					fprintf(out, "`MOV,`SHR,");
 					print_where(out, addr1);
 					print_where(out, addr2);
 					fprintf(out, "`N8");
 				}
 				break;
-			case 0x504d4a: //JMP UNC (JMP)
+			case 0x504d4a: // JMP UNC (JMP)
 				if (i) {
-					fprintf(out,"`JMP,`UNC,");
-					fprintf(out,"`N10,`N10,");
-					print_bits(out,atoi(addr1)-1);
+					fprintf(out, "`JMP,`UNC,");
+					fprintf(out, "`N10,`N10,");
+					print_bits(out, atoi(addr1) - 1);
 				}
 				break;
-			case 0x51454a: //JMP EQ (JEQ)
+			case 0x51454a: // JMP EQ (JEQ)
 				if (i) {
-					fprintf(out,"`JMP,`EQ,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					print_bits(out,atoi(addr3)-1);
-				}
-				break;
-			case 0x544c554a: //JMP ULT (JULT)
-				if (i) {
-					fprintf(out,"`JMP,`ULT,");
+					fprintf(out, "`JMP,`EQ,");
 					print_where(out, addr1);
 					print_where(out, addr2);
-					print_bits(out, atoi(addr3)-1);
+					print_bits(out, atoi(addr3) - 1);
 				}
 				break;
-			case 0x544c534a: //JMP SLT (JSLT)
+			case 0x544c554a: // JMP ULT (JULT)
 				if (i) {
-					fprintf(out,"`JMP,`SLT,");
+					fprintf(out, "`JMP,`ULT,");
 					print_where(out, addr1);
 					print_where(out, addr2);
-					print_bits(out, atoi(addr3)-1);
+					print_bits(out, atoi(addr3) - 1);
 				}
 				break;
-			case 0x454c554a: //JMP ULE (JULE)
+			case 0x544c534a: // JMP SLT (JSLT)
 				if (i) {
-					fprintf(out,"`JMP,`ULE,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					print_bits(out,atoi(addr3)-1);
+					fprintf(out, "`JMP,`SLT,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					print_bits(out, atoi(addr3) - 1);
 				}
 				break;
-			case 0x454c534a: //JMP SLE (JSLE)
+			case 0x454c554a: // JMP ULE (JULE)
 				if (i) {
-					fprintf(out,"`JMP,`SLE,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					print_bits(out,atoi(addr3)-1);
+					fprintf(out, "`JMP,`ULE,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					print_bits(out, atoi(addr3) - 1);
 				}
 				break;
-			case 0x444155: //ACC UAD (UAD)
+			case 0x454c534a: // JMP SLE (JSLE)
 				if (i) {
-					fprintf(out,"`ACC,`UAD,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`JMP,`SLE,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					print_bits(out, atoi(addr3) - 1);
 				}
 				break;
-			case 0x444153: //ACC SAD (SAD)
+			case 0x444155: // ACC UAD (UAD)
 				if (i) {
-					fprintf(out,"`ACC,`SAD,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`ACC,`UAD,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
 				}
 				break;
-			case 0x544d55: //ACC UMT (UMT)
+			case 0x444153: // ACC SAD (SAD)
 				if (i) {
-					fprintf(out,"`ACC,`UMT,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`ACC,`SAD,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
 				}
 				break;
-			case 0x544d53: //ACC SMT (SMT)
+			case 0x544d55: // ACC UMT (UMT)
 				if (i) {
-					fprintf(out,"`ACC,`SMT,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`ACC,`UMT,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
 				}
 				break;
-			case 0x444e41: //ACC AND (AND)
+			case 0x544d53: // ACC SMT (SMT)
 				if (i) {
-					fprintf(out,"`ACC,`AND,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`ACC,`SMT,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
 				}
 				break;
-			case 0x524f: //ACC OR (OR)
+			case 0x444e41: // ACC AND (AND)
 				if (i) {
-					fprintf(out,"`ACC,`OR,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`ACC,`AND,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
 				}
 				break;
-			case 0x524f58: //ACC XOR (XOR)
+			case 0x524f: // ACC OR (OR)
 				if (i) {
-					fprintf(out,"`ACC,`XOR,");
-					print_where(out,addr1);
-					print_where(out,addr2);
-					fprintf(out,"`N8");
+					fprintf(out, "`ACC,`OR,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
+				}
+				break;
+			case 0x524f58: // ACC XOR (XOR)
+				if (i) {
+					fprintf(out, "`ACC,`XOR,");
+					print_where(out, addr1);
+					print_where(out, addr2);
+					fprintf(out, "`N8");
 				}
 				break;
 			default:
-				printf("ERROR: Invalid command %s\n",cmd);
-				invalid_flag=1;
+				printf("ERROR: Invalid command %s\n", cmd);
+				invalid_flag = 1;
 				break;
 			}
 			if (i)
-				fprintf(out,"};\n");
+				fprintf(out, "};\n");
 		}
 		if (invalid_flag && !i) {
 			printf("There are errors, please fix them before contiuing\n");
 			exit(0);
 		}
 		if (i) {
-			fprintf(out,"\t\t\tdefault: data = 35'b0;\n");
-			fprintf(out,"\tendcase\nendmodule");
+			fprintf(out, "\t\t\tdefault: data = 35'b0;\n");
+			fprintf(out, "\tendcase\nendmodule");
 		}
 		rewind(in);
 	}
@@ -216,61 +222,61 @@ int main(int argc, char **argv)
 
 void print_bits(FILE *out, int num)
 {
-	fprintf(out,"8'd%d",num);
+	fprintf(out, "8'd%d", num);
 }
 
-void print_where(FILE *out, char* arg)
+void print_where(FILE *out, char *arg)
 {
 	if (arg[0] == 'R') {
 		fprintf(out, "`REG,");
-		print_bits(out, atoi(arg+1));
-		fprintf(out,",");
+		print_bits(out, atoi(arg + 1));
+		fprintf(out, ",");
 	} else if (arg[0] == '*') {
 		fprintf(out, "`IND,");
-		print_bits(out, atoi(arg+1));
-		fprintf(out,",");
+		print_bits(out, atoi(arg + 1));
+		fprintf(out, ",");
 	} else if (arg[0] == 'O') {
-		fprintf(out,"`REG,`DOUT,");
+		fprintf(out, "`REG,`DOUT,");
 	} else if (arg[0] == 'I') {
-		fprintf(out,"`REG,`DINP,");
+		fprintf(out, "`REG,`DINP,");
 	} else if (arg[0] == 'G') {
-		fprintf(out,"`REG,`GOUT,");
+		fprintf(out, "`REG,`GOUT,");
 	} else {
 		fprintf(out, "`NUM,");
 		print_bits(out, atoi(arg));
-		fprintf(out,",");
+		fprintf(out, ",");
 	}
 }
 
 int print_loc(FILE *out, char *loc, int run)
 {
-	switch (*((int *) loc)) {
-	case 0x4c504d53: //SMPL
+	switch (*((int *)loc)) {
+	case 0x4c504d53: // SMPL
 		if (run)
-			fprintf(out,"`SMPL,");
+			fprintf(out, "`SMPL,");
 		break;
-	case 0x54464853: //SHFT
+	case 0x54464853: // SHFT
 		if (run)
-			fprintf(out,"`SHFT,");
+			fprintf(out, "`SHFT,");
 		break;
-	case 0x574c464f: //OFLW
+	case 0x574c464f: // OFLW
 		if (run)
-			fprintf(out,"`OFLW,");
+			fprintf(out, "`OFLW,");
 		break;
-	case 0x324e5442: //BTN2
+	case 0x324e5442: // BTN2
 		if (run)
-			fprintf(out,"3'b010,");
+			fprintf(out, "3'b010,");
 		break;
-	case 0x314e5442: //BTN1
+	case 0x314e5442: // BTN1
 		if (run)
-			fprintf(out,"3'b001,");
+			fprintf(out, "3'b001,");
 		break;
-	case 0x304e5442: //BTN0
+	case 0x304e5442: // BTN0
 		if (run)
-			fprintf(out,"3'b000,");
+			fprintf(out, "3'b000,");
 		break;
 	default:
-		printf("ERROR: Invalid flag: %s, %x",loc, *((int *)loc));
+		printf("ERROR: Invalid flag: %s, %x", loc, *((int *)loc));
 		return 1;
 		break;
 	}
--- interpreter.c
+++ interpreter.c
@@ -20,159 +20,159 @@ char *get_loc(char *Reg, char *addr, int IP);
 char get_num(char *Reg, char *addr);
 int print_loc(char *loc);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	FILE *in;
-	in = fopen(argv[1],"r");
-	char IP=0;
+	in = fopen(argv[1], "r");
+	char IP = 0;
 	struct instruction ins[256];
 	char Reg[32];
 	char *DINP = Reg + 28;
 	char *GOUT = Reg + 29;
 	char *DOUT = Reg + 30;
 	char *FLAG = Reg + 31;
-	char ad1,ad2,ad3,*loc;
+	char ad1, ad2, ad3, *loc;
 	int temp;
+	*DOUT = 0;
+	*DINP = 0;
+	*GOUT = 0;
 
-	parser(in,ins);
+	parser(in, ins);
 
 	while (1) {
-		switch (*((int *) ins[IP].cmd)) {
-		case 0x504f4e: //NOP (NOP)
+		switch (*((int *)ins[IP].cmd)) {
+		case 0x504f4e: // NOP (NOP)
 			break;
-		case 0x435441: //ATC (ATC)
-			if (*FLAG & 1<<print_loc(ins[IP].addr1)) {
-				*FLAG &= ~(1<<print_loc(ins[IP].addr2));
+		case 0x435441: // ATC (ATC)
+			if (*FLAG & 1 << print_loc(ins[IP].addr1)) {
+				*FLAG &= ~(1 << print_loc(ins[IP].addr2));
 				IP = atoi(ins[IP].addr2);
 				continue;
 			}
-			*FLAG &= ~(1<<print_loc(ins[IP].addr2));
+			*FLAG &= ~(1 << print_loc(ins[IP].addr2));
 			break;
-		case 0x564f4d: //MOV PUR (MOV)
-			ad1=get_num(Reg, ins[IP].addr1);
-			loc=get_loc(Reg, ins[IP].addr2,IP);
+		case 0x564f4d: // MOV PUR (MOV)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			loc = get_loc(Reg, ins[IP].addr2, IP);
 			*loc = ad1;
 			break;
-		case 0x4c4853: //MOV SHL (SHL)
-			ad1=get_num(Reg,ins[IP].addr1);
-			loc=get_loc(Reg,ins[IP].addr2,IP);
-			*loc=((unsigned char)ad1)<<1;
+		case 0x4c4853: // MOV SHL (SHL)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			loc = get_loc(Reg, ins[IP].addr2, IP);
+			*loc = ((unsigned char)ad1) << 1;
 			break;
-		case 0x524853: //MOV SHR (SHR)
-			ad1=get_num(Reg,ins[IP].addr1);
-			loc=get_loc(Reg,ins[IP].addr2,IP);
-			*loc=((unsigned char)ad1)>>1;
+		case 0x524853: // MOV SHR (SHR)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			loc = get_loc(Reg, ins[IP].addr2, IP);
+			*loc = ((unsigned char)ad1) >> 1;
 			break;
-		case 0x504d4a: //JMP UNC (JMP)
-			ad1=get_num(Reg, ins[IP].addr1);
-			IP=ad1;
+		case 0x504d4a: // JMP UNC (JMP)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			IP = ad1;
 			continue;
 			break;
-		case 0x51454a: //JMP EQ (JEQ)
-			ad1=get_num(Reg,ins[IP].addr1);
-			ad2=get_num(Reg,ins[IP].addr2);
-			ad3=get_num(Reg,ins[IP].addr3);
+		case 0x51454a: // JMP EQ (JEQ)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			ad2 = get_num(Reg, ins[IP].addr2);
+			ad3 = get_num(Reg, ins[IP].addr3);
 			if ((ad1 == ad2)) {
 				IP = ad3;
 				continue;
 			}
 			break;
-		case 0x544c554a: //JMP ULT (JULT)
-			ad1=get_num(Reg,ins[IP].addr1);
-			ad2=get_num(Reg,ins[IP].addr2);
-			ad3=get_num(Reg,ins[IP].addr3);
+		case 0x544c554a: // JMP ULT (JULT)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			ad2 = get_num(Reg, ins[IP].addr2);
+			ad3 = get_num(Reg, ins[IP].addr3);
 			if ((unsigned char)ad1 < (unsigned char)ad2) {
 				IP = ad3;
 				continue;
 			}
 			break;
-		case 0x544c534a: //JMP SLT (JSLT)
-			ad1=get_num(Reg,ins[IP].addr1);
-			ad2=get_num(Reg,ins[IP].addr2);
-			ad3=get_num(Reg,ins[IP].addr3);
+		case 0x544c534a: // JMP SLT (JSLT)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			ad2 = get_num(Reg, ins[IP].addr2);
+			ad3 = get_num(Reg, ins[IP].addr3);
 			if (ad1 < ad2) {
 				IP = ad3;
 				continue;
 			}
 			break;
-		case 0x454c554a: //JMP ULE (JULE)
-			ad1=get_num(Reg,ins[IP].addr1);
-			ad2=get_num(Reg,ins[IP].addr2);
-			ad3=get_num(Reg,ins[IP].addr3);
+		case 0x454c554a: // JMP ULE (JULE)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			ad2 = get_num(Reg, ins[IP].addr2);
+			ad3 = get_num(Reg, ins[IP].addr3);
 			if ((unsigned char)ad1 <= (unsigned char)ad2) {
 				IP = ad3;
 				continue;
 			}
 			break;
-		case 0x454c534a: //JMP SLE (JSLE)
-			ad1=get_num(Reg,ins[IP].addr1);
-			ad2=get_num(Reg,ins[IP].addr2);
-			ad3=get_num(Reg,ins[IP].addr3);
+		case 0x454c534a: // JMP SLE (JSLE)
+			ad1 = get_num(Reg, ins[IP].addr1);
+			ad2 = get_num(Reg, ins[IP].addr2);
+			ad3 = get_num(Reg, ins[IP].addr3);
 			if (ad1 <= ad2) {
 				IP = ad3;
 				continue;
 			}
 			break;
-		case 0x444155: //ACC UAD (UAD)
-			loc=get_loc(Reg,ins[IP].addr1,IP);
-			ad1=get_num(Reg,ins[IP].addr2);
+		case 0x444155: // ACC UAD (UAD)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			temp = *loc = (unsigned char)(*loc) + (unsigned char)ad1;
 			if (temp > 255)
-				*FLAG |= 1<<OFLW;
+				*FLAG |= 1 << OFLW;
 			break;
-		case 0x444153: //ACC SAD (SAD)
-			loc=get_loc(Reg,ins[IP].addr1,IP);
-			ad1=get_num(Reg,ins[IP].addr2);
+		case 0x444153: // ACC SAD (SAD)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			temp = *loc = (*loc) + ad1;
 			if (temp < -128 || temp > 127)
-				*FLAG |= 1<<OFLW;
+				*FLAG |= 1 << OFLW;
 			break;
-		case 0x544d55: //ACC UMT (UMT)
-			loc=get_loc(Reg,ins[IP].addr1,IP);
-			ad1=get_num(Reg,ins[IP].addr2);
+		case 0x544d55: // ACC UMT (UMT)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			temp = *loc = (unsigned char)(*loc) * (unsigned char)ad1;
 			if (temp > 255)
-				*FLAG |= 1<<OFLW;
+				*FLAG |= 1 << OFLW;
 			break;
-		case 0x544d53: //ACC SMT (SMT)
-			loc=get_loc(Reg,ins[IP].addr1,IP);
-			ad1=get_num(Reg,ins[IP].addr2);
+		case 0x544d53: // ACC SMT (SMT)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			temp = *loc = (*loc) * ad1;
 			if (temp < -128 || temp > 127)
-				*FLAG |= 1<<OFLW;
+				*FLAG |= 1 << OFLW;
 			break;
-		case 0x444e41: //ACC AND (AND)
-			loc = get_loc(Reg,ins[IP].addr1,IP);
-			ad1 = get_num(Reg,ins[IP].addr2);
+		case 0x444e41: // ACC AND (AND)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			*loc &= ad1;
 			break;
-		case 0x524f: //ACC OR (OR)
-			loc = get_loc(Reg,ins[IP].addr1,IP);
-			ad1 = get_num(Reg,ins[IP].addr2);
+		case 0x524f: // ACC OR (OR)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			*loc |= ad1;
 			break;
-		case 0x524f58: //ACC XOR (XOR)
-			loc = get_loc(Reg,ins[IP].addr1,IP);
-			ad1 = get_num(Reg,ins[IP].addr2);
+		case 0x524f58: // ACC XOR (XOR)
+			loc = get_loc(Reg, ins[IP].addr1, IP);
+			ad1 = get_num(Reg, ins[IP].addr2);
 			*loc ^= ad1;
 			break;
 		default:
-			printf("ERROR: Invalid command %s\n",ins[IP].cmd);
+			printf("ERROR: Invalid command %s\n", ins[IP].cmd);
 			exit(2);
 			break;
 		}
-		printf("%d %d\n",IP-1,*DOUT);
+		printf("%d %d\n", IP - 1, *DOUT);
 		IP++;
 	}
 }
 
-
-int parser(FILE *in, struct instruction *ins)
-{
+int parser(FILE *in, struct instruction *ins) {
 	char line[1024];
 	int i = 0;
 	for (i = 0; fgets(line, 1024, in); i++) {
-		int ctr=0,cmdcnt=0,ad1cnt=0,ad2cnt=0,ad3cnt=0;
+		int ctr = 0, cmdcnt = 0, ad1cnt = 0, ad2cnt = 0, ad3cnt = 0;
 		for (int j = 0; line[j]; j++) {
 			if (line[j] == '\t') {
 				continue;
@@ -184,19 +184,26 @@ int parser(FILE *in, struct instruction *ins)
 			if (line[i] == ';' || line[i] == '/')
 				break;
 			switch (ctr) {
-			case 0: ins[i].cmd[cmdcnt++] = line[j];
+			case 0:
+				ins[i].cmd[cmdcnt++] = line[j];
 				break;
-			case 1: ins[i].addr1[ad1cnt++] = line[j];
+			case 1:
+				ins[i].addr1[ad1cnt++] = line[j];
 				break;
-			case 2: ins[i].addr2[ad2cnt++] = line[j];
+			case 2:
+				ins[i].addr2[ad2cnt++] = line[j];
 				break;
-			case 3: ins[i].addr3[ad3cnt++] = line[j];
+			case 3:
+				ins[i].addr3[ad3cnt++] = line[j];
 				break;
 			default:
 				break;
 			}
 		}
-		ins[i].cmd[cmdcnt] = '\0'; ins[i].addr1[ad1cnt] = '\0'; ins[i].addr2[ad2cnt] = '\0'; ins[i].addr3[ad3cnt] = '\0';
+		ins[i].cmd[cmdcnt] = '\0';
+		ins[i].addr1[ad1cnt] = '\0';
+		ins[i].addr2[ad2cnt] = '\0';
+		ins[i].addr3[ad3cnt] = '\0';
 		if (cmdcnt == 3)
 			ins[i].cmd[++cmdcnt] = '\0';
 		if (cmdcnt == 4)
@@ -205,28 +212,26 @@ int parser(FILE *in, struct instruction *ins)
 	return 1;
 }
 
-char *get_loc(char *Reg, char *addr, int IP)
-{
+char *get_loc(char *Reg, char *addr, int IP) {
 	if (addr[0] == 'R') {
-		return Reg+atoi(addr+1);
+		return Reg + atoi(addr + 1);
 	} else if (addr[0] == '*') {
-		return Reg + Reg[atoi(addr+1)];
+		return Reg + Reg[atoi(addr + 1)];
 	} else if (addr[0] == 'I') {
-		return Reg+28;
+		return Reg + 28;
 	} else if (addr[0] == 'O') {
-		return Reg+30;
+		return Reg + 30;
 	} else {
-		printf("Error: Line %d, trying to move into a number",IP);
+		printf("Error: Line %d, trying to move into a number", IP);
 		exit(0);
 	}
 }
 
-char get_num(char *Reg, char *addr)
-{
+char get_num(char *Reg, char *addr) {
 	if (addr[0] == 'R')
-		return Reg[atoi(addr+1)];
+		return Reg[atoi(addr + 1)];
 	else if (addr[0] == '*')
-		return Reg[(int)Reg[atoi(addr+1)]];
+		return Reg[(int)Reg[atoi(addr + 1)]];
 	else if (addr[0] == 'I')
 		return Reg[28];
 	else if (addr[0] == 'O')
@@ -235,29 +240,28 @@ char get_num(char *Reg, char *addr)
 		return atoi(addr);
 }
 
-int print_loc(char *loc)
-{
-	switch (*((int *) loc)) {
-	case 0x4c504d53: //SMPL
+int print_loc(char *loc) {
+	switch (*((int *)loc)) {
+	case 0x4c504d53: // SMPL
 		return SMPL;
 		break;
-	case 0x54464853: //SHFT
+	case 0x54464853: // SHFT
 		return SHFT;
 		break;
-	case 0x574c464f: //OFLW
+	case 0x574c464f: // OFLW
 		return OFLW;
 		break;
-	case 0x42544e32: //BTN2
+	case 0x42544e32: // BTN2
 		return BTN2;
 		break;
-	case 0x314e5442: //BTN1
+	case 0x314e5442: // BTN1
 		return BTN1;
 		break;
-	case 0x304e5442: //BTN0
+	case 0x304e5442: // BTN0
 		return BTN0;
 		break;
 	default:
-		printf("ERROR: Invalid flag: %s",loc);
+		printf("ERROR: Invalid flag: %s", loc);
 		exit(1);
 		break;
 	}
